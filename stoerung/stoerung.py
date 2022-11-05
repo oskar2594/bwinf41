98acd0d13cfa6aa887bea7b9da7f242a4dd289a2
Anton1501 <67712068+Anton1501@users.noreply.github.com>
2022-11-05 19:35:42 +0100

    quasi fertig :)

--- stoerung/stoerung.py
+++ stoerung/stoerung.py
@@ -1,43 +1,36 @@
-
 with open("Alice_im_Wunderland.txt", 'r') as read_obj:
-    with open("stoerung0.txt", "r") as x:
+    with open("stoerung3.txt", "r") as x:
         line_number = 0
         list_of_results = []
 
         buch = list(filter(None, read_obj.readlines()))
 
-        schnipsel = x.readline().split(" ")
+        stoerung = x.readline().split(" ")
         arr = []
-        loesung = []
-        print(len(schnipsel))
+
         while line_number < len(buch):
             arr += buch[line_number].split(" ")
-            while len(arr) > len(schnipsel):
-                for i, word in enumerate(schnipsel):
-                    if i + 1 == len(schnipsel):
-                        loesung.append(line_number)
-                        print(arr)
-                        for g in range(len(schnipsel)):
-                            arr.pop(0)
-                        break
-                    if word in arr[i] or word == "_":
+            while len(arr) > len(stoerung):
+                for i, word in enumerate(stoerung):
+                    if word in arr[i] or word.capitalize() in arr[i] or word == "_":
                         pass
                     else:
                         arr.pop(0)
                         break
+                    if i == len(stoerung) - 1:
+                        # hier ist die lösung gefunden worden
+                        # man müsste die lösung noch ein bisschen putzen und von allen sonderzeichen befreien, sonst top
+                        list_of_results.append((arr[:len(stoerung)], line_number))
+
+                        # auch mit: arr = arr[len(stoerung):]
+                        for g in range(len(stoerung)):
+                            arr.pop(0)
+                            
+                        break
 
             line_number += 1
 
-for i in loesung:
-    print(buch[i])
-
-
-
-
-
-
-
-
-
-
-
+for x, y in list_of_results:
+    print("line: " + str(y))
+    print(x)
+    print("")
