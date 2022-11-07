e6f98a21421db3bb29eb1edf41df3669b8c476f8
phgilde <ph.gilde@gmail.com>
2022-11-07 21:56:04 +0100

    pointfree und standardbibliothek

--- huepfburg/huepfburg.hs
+++ huepfburg/huepfburg.hs
@@ -9,11 +9,8 @@ type VertexPair = (Vertex, Vertex)
 type OriginFunc = VertexPair -> VertexPair
 type State = (Set Vertex, Set Vertex, Set VertexPair, OriginFunc)
 
-flatten :: [[a]] -> [a]
-flatten = foldl1 (++)
-
 nextPos :: Graph -> Set Vertex -> Set Vertex
-nextPos graph pos = fromList $ flatten [graph ! p | p <- elems pos]
+nextPos graph pos = fromList $ concat [graph ! p | p <- elems pos]
 
 nextState :: Graph -> State -> State
 nextState graph (pos1, pos2, pairs, origin) =
@@ -29,8 +26,7 @@ nextState graph (pos1, pos2, pairs, origin) =
 
 -- Findet den Weg zu einem Knotenpaar aus einer Herkunftsfunktion
 startPath :: OriginFunc -> VertexPair -> [VertexPair]
-startPath _ (1, 2) = [(1, 2)]
-startPath origin (p1, p2) = (p1, p2) : startPath origin (origin (p1, p2))
+startPath origin = takeWhile (/=(1, 2)) . iterate origin 
 
 solve :: Graph -> State -> Maybe [VertexPair]
 solve graph (pos1, pos2, pairs, origin)
