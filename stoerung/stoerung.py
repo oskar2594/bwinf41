ccf5a59a44e5bdfca626df090e2b011c9d3f70a1
phgilde <ph.gilde@gmail.com>
2022-11-16 21:42:37 +0100

    bisschen geordnet

--- stoerung/stoerung.py
+++ stoerung/stoerung.py
@@ -1,36 +1,37 @@
-with open("Alice_im_Wunderland.txt", 'r') as read_obj:
-    with open("stoerung3.txt", "r") as x:
-        line_number = 0
-        list_of_results = []
+# Buch einlesen
+with open("Alice_im_Wunderland.txt", encoding="utf-8") as file:
+    buch = list(filter(None, file.readlines()))
 
-        buch = list(filter(None, read_obj.readlines()))
+# Gestörte Nachricht einlesen
+with open(input("Pfad: ")) as file:
+    stoerung = file.readline().split(" ")
 
-        stoerung = x.readline().split(" ")
-        arr = []
+line_number = 0
+list_of_results = []
+arr = []
 
-        while line_number < len(buch):
-            arr += buch[line_number].split(" ")
-            while len(arr) > len(stoerung):
-                for i, word in enumerate(stoerung):
-                    if word in arr[i] or word.capitalize() in arr[i] or word == "_":
-                        pass
-                    else:
-                        arr.pop(0)
-                        break
-                    if i == len(stoerung) - 1:
-                        # hier ist die lösung gefunden worden
-                        # man müsste die lösung noch ein bisschen putzen und von allen sonderzeichen befreien, sonst top
-                        list_of_results.append((arr[:len(stoerung)], line_number))
+while line_number < len(buch):
+    arr += buch[line_number].split(" ")
+    while len(arr) > len(stoerung):
+        for i, word in enumerate(stoerung):
+            if not (word in arr[i] or word.capitalize() in arr[i] or word == "_"):
+                arr.pop(0)
+                break
+            if i == len(stoerung) - 1:
+                # hier ist die lösung gefunden worden
+                # man müsste die lösung noch ein bisschen putzen 
+                # und von allen sonderzeichen befreien, sonst top
+                list_of_results.append((arr[: len(stoerung)], line_number))
 
-                        # auch mit: arr = arr[len(stoerung):]
-                        for g in range(len(stoerung)):
-                            arr.pop(0)
-                            
-                        break
+                # auch mit: arr = arr[len(stoerung):]
+                for g in range(len(stoerung)):
+                    arr.pop(0)
 
-            line_number += 1
+                break
 
-for x, y in list_of_results:
-    print("line: " + str(y))
-    print(x)
+    line_number += 1
+
+for line, line_number in list_of_results:
+    print("line: " + str(line_number))
+    print(line)
     print("")
